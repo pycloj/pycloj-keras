a887012f23a789bf3eb54fe2da0e11491c64d3cb
david <davidvoler@gmail.com>
2019-11-18 15:40:33 +0200

    pushed to clojars

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 
-(defproject pyclj-keras "2.2.4"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject pycloj/pycloj-keras "2.2.4-AUTO-0.1-SNAPSHOT"
+  :description "Auto generated clojure bridge to keras "
+  :url "https://github.com/pycloj/pycloj-keras"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"] [libpython-clj "1.6-SNAPSHOT"][alembic "0.3.2"]]
