7d3c5f22a51567518aeb497ff0ce78a79129bf35
david <davidvoler@gmail.com>
2019-11-19 11:36:23 +0200

    correcting reference

--- src/keras/backend/sequential.clj
+++ src/keras/backend/sequential.clj
@@ -1,6 +1,128 @@
 
-(ns keras.engine.sequential
+(ns keras.backend.Sequential
   (:require [keras.engine.sequential]))
 
-(defonce Sequential keras.engine.sequential/Sequential)
+(defonce Sequential keras.engine.sequential.Sequential/Sequential)
 
+
+(defonce add keras.engine.sequential.Sequential/add)
+
+(defonce add-loss keras.engine.sequential.Sequential/add-loss)
+
+(defonce add-update keras.engine.sequential.Sequential/add-update)
+
+(defonce add-weight keras.engine.sequential.Sequential/add-weight)
+
+(defonce assert-input-compatibility keras.engine.sequential.Sequential/assert-input-compatibility)
+
+(defonce build keras.engine.sequential.Sequential/build)
+
+(defonce built keras.engine.sequential.Sequential/built)
+
+(defonce call keras.engine.sequential.Sequential/call)
+
+(defonce compile keras.engine.sequential.Sequential/compile)
+
+(defonce compute-mask keras.engine.sequential.Sequential/compute-mask)
+
+(defonce compute-output-shape keras.engine.sequential.Sequential/compute-output-shape)
+
+(defonce count-params keras.engine.sequential.Sequential/count-params)
+
+(defonce evaluate keras.engine.sequential.Sequential/evaluate)
+
+(defonce evaluate-generator keras.engine.sequential.Sequential/evaluate-generator)
+
+(defonce fit keras.engine.sequential.Sequential/fit)
+
+(defonce fit-generator keras.engine.sequential.Sequential/fit-generator)
+
+(defonce get-config keras.engine.sequential.Sequential/get-config)
+
+(defonce get-input-at keras.engine.sequential.Sequential/get-input-at)
+
+(defonce get-input-mask-at keras.engine.sequential.Sequential/get-input-mask-at)
+
+(defonce get-input-shape-at keras.engine.sequential.Sequential/get-input-shape-at)
+
+(defonce get-layer keras.engine.sequential.Sequential/get-layer)
+
+(defonce get-losses-for keras.engine.sequential.Sequential/get-losses-for)
+
+(defonce get-output-at keras.engine.sequential.Sequential/get-output-at)
+
+(defonce get-output-mask-at keras.engine.sequential.Sequential/get-output-mask-at)
+
+(defonce get-output-shape-at keras.engine.sequential.Sequential/get-output-shape-at)
+
+(defonce get-updates-for keras.engine.sequential.Sequential/get-updates-for)
+
+(defonce get-weights keras.engine.sequential.Sequential/get-weights)
+
+(defonce input keras.engine.sequential.Sequential/input)
+
+(defonce input-mask keras.engine.sequential.Sequential/input-mask)
+
+(defonce input-shape keras.engine.sequential.Sequential/input-shape)
+
+(defonce input-spec keras.engine.sequential.Sequential/input-spec)
+
+(defonce layers keras.engine.sequential.Sequential/layers)
+
+(defonce load-weights keras.engine.sequential.Sequential/load-weights)
+
+(defonce losses keras.engine.sequential.Sequential/losses)
+
+(defonce model keras.engine.sequential.Sequential/model)
+
+(defonce non-trainable-weights keras.engine.sequential.Sequential/non-trainable-weights)
+
+(defonce output keras.engine.sequential.Sequential/output)
+
+(defonce output-mask keras.engine.sequential.Sequential/output-mask)
+
+(defonce output-shape keras.engine.sequential.Sequential/output-shape)
+
+(defonce pop keras.engine.sequential.Sequential/pop)
+
+(defonce predict keras.engine.sequential.Sequential/predict)
+
+(defonce predict-classes keras.engine.sequential.Sequential/predict-classes)
+
+(defonce predict-generator keras.engine.sequential.Sequential/predict-generator)
+
+(defonce predict-on-batch keras.engine.sequential.Sequential/predict-on-batch)
+
+(defonce predict-proba keras.engine.sequential.Sequential/predict-proba)
+
+(defonce reset-states keras.engine.sequential.Sequential/reset-states)
+
+(defonce run-internal-graph keras.engine.sequential.Sequential/run-internal-graph)
+
+(defonce save keras.engine.sequential.Sequential/save)
+
+(defonce save-weights keras.engine.sequential.Sequential/save-weights)
+
+(defonce set-weights keras.engine.sequential.Sequential/set-weights)
+
+(defonce state-updates keras.engine.sequential.Sequential/state-updates)
+
+(defonce stateful keras.engine.sequential.Sequential/stateful)
+
+(defonce summary keras.engine.sequential.Sequential/summary)
+
+(defonce test-on-batch keras.engine.sequential.Sequential/test-on-batch)
+
+(defonce to-json keras.engine.sequential.Sequential/to-json)
+
+(defonce to-yaml keras.engine.sequential.Sequential/to-yaml)
+
+(defonce train-on-batch keras.engine.sequential.Sequential/train-on-batch)
+
+(defonce trainable-weights keras.engine.sequential.Sequential/trainable-weights)
+
+(defonce updates keras.engine.sequential.Sequential/updates)
+
+(defonce uses-learning-phase keras.engine.sequential.Sequential/uses-learning-phase)
+
+(defonce weights keras.engine.sequential.Sequential/weights)
--- src/keras/layers/wrappers/training.clj
+++ src/keras/layers/wrappers/training.clj
@@ -1,6 +1,118 @@
 
-(ns keras.engine.training
+(ns keras.layers.wrappers.Model
   (:require [keras.engine.training]))
 
-(defonce Model keras.engine.training/Model)
+(defonce Model keras.engine.training.Model/Model)
 
+
+(defonce add-loss keras.engine.training.Model/add-loss)
+
+(defonce add-update keras.engine.training.Model/add-update)
+
+(defonce add-weight keras.engine.training.Model/add-weight)
+
+(defonce assert-input-compatibility keras.engine.training.Model/assert-input-compatibility)
+
+(defonce build keras.engine.training.Model/build)
+
+(defonce built keras.engine.training.Model/built)
+
+(defonce call keras.engine.training.Model/call)
+
+(defonce compile keras.engine.training.Model/compile)
+
+(defonce compute-mask keras.engine.training.Model/compute-mask)
+
+(defonce compute-output-shape keras.engine.training.Model/compute-output-shape)
+
+(defonce count-params keras.engine.training.Model/count-params)
+
+(defonce evaluate keras.engine.training.Model/evaluate)
+
+(defonce evaluate-generator keras.engine.training.Model/evaluate-generator)
+
+(defonce fit keras.engine.training.Model/fit)
+
+(defonce fit-generator keras.engine.training.Model/fit-generator)
+
+(defonce get-config keras.engine.training.Model/get-config)
+
+(defonce get-input-at keras.engine.training.Model/get-input-at)
+
+(defonce get-input-mask-at keras.engine.training.Model/get-input-mask-at)
+
+(defonce get-input-shape-at keras.engine.training.Model/get-input-shape-at)
+
+(defonce get-layer keras.engine.training.Model/get-layer)
+
+(defonce get-losses-for keras.engine.training.Model/get-losses-for)
+
+(defonce get-output-at keras.engine.training.Model/get-output-at)
+
+(defonce get-output-mask-at keras.engine.training.Model/get-output-mask-at)
+
+(defonce get-output-shape-at keras.engine.training.Model/get-output-shape-at)
+
+(defonce get-updates-for keras.engine.training.Model/get-updates-for)
+
+(defonce get-weights keras.engine.training.Model/get-weights)
+
+(defonce input keras.engine.training.Model/input)
+
+(defonce input-mask keras.engine.training.Model/input-mask)
+
+(defonce input-shape keras.engine.training.Model/input-shape)
+
+(defonce input-spec keras.engine.training.Model/input-spec)
+
+(defonce layers keras.engine.training.Model/layers)
+
+(defonce load-weights keras.engine.training.Model/load-weights)
+
+(defonce losses keras.engine.training.Model/losses)
+
+(defonce non-trainable-weights keras.engine.training.Model/non-trainable-weights)
+
+(defonce output keras.engine.training.Model/output)
+
+(defonce output-mask keras.engine.training.Model/output-mask)
+
+(defonce output-shape keras.engine.training.Model/output-shape)
+
+(defonce predict keras.engine.training.Model/predict)
+
+(defonce predict-generator keras.engine.training.Model/predict-generator)
+
+(defonce predict-on-batch keras.engine.training.Model/predict-on-batch)
+
+(defonce reset-states keras.engine.training.Model/reset-states)
+
+(defonce run-internal-graph keras.engine.training.Model/run-internal-graph)
+
+(defonce save keras.engine.training.Model/save)
+
+(defonce save-weights keras.engine.training.Model/save-weights)
+
+(defonce set-weights keras.engine.training.Model/set-weights)
+
+(defonce state-updates keras.engine.training.Model/state-updates)
+
+(defonce stateful keras.engine.training.Model/stateful)
+
+(defonce summary keras.engine.training.Model/summary)
+
+(defonce test-on-batch keras.engine.training.Model/test-on-batch)
+
+(defonce to-json keras.engine.training.Model/to-json)
+
+(defonce to-yaml keras.engine.training.Model/to-yaml)
+
+(defonce train-on-batch keras.engine.training.Model/train-on-batch)
+
+(defonce trainable-weights keras.engine.training.Model/trainable-weights)
+
+(defonce updates keras.engine.training.Model/updates)
+
+(defonce uses-learning-phase keras.engine.training.Model/uses-learning-phase)
+
+(defonce weights keras.engine.training.Model/weights)
